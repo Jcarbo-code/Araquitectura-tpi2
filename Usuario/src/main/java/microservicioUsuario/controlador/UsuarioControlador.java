3849018f7fe50dfc2c5c04dbc781a3f5295fe045
Nicolas Muda <nicolasmuda@live.com.ar>
2023-11-17 17:03:46 -0300

    se agrego swagger

--- Usuario/src/main/java/microservicioUsuario/controlador/UsuarioControlador.java
+++ Usuario/src/main/java/microservicioUsuario/controlador/UsuarioControlador.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import microservicioUsuario.dtos.ReporteUso;
 import microservicioUsuario.dtos.UsuarioDto;
 import microservicioUsuario.modelo.Usuario;
@@ -20,6 +21,7 @@ import microservicioUsuario.servicio.UsuarioServicio;
 
 @RestController
 @RequestMapping("/Usuario")
+@Tag(name = "Servicio usuario", description = "se encarga de todo lo referente a los usuarios y la generacion de reportes")
 public class UsuarioControlador {
 
 	@Autowired
